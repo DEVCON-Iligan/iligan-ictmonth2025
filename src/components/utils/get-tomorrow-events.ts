a608e29cddd25ab3cfbf1b29923123aef1128e4e
jian6137 <lbd6137@gmail.com>
2025-06-17 12:03:36 +0800

    Added a get-tomorrow-events.ts util function to get all events that will happen 1 day after the client's local date & time

---
+++ src/components/utils/get-tomorrow-events.ts
@@ -0,0 +1,54 @@
+type EventType = {
+  title: string;
+  date: string;
+  time: {
+    start: string;
+    end: string;
+  };
+  location: string;
+  attendees: number;
+  description: string;
+  type: string;
+  agencies: { name: string; logo: string }[] | string[];
+  registerLink: string;
+  posts: {
+    poster: string;
+    title: string;
+    description: string;
+    link: string;
+    thumbnail: string;
+  }[];
+};
+
+export function getNextDayEventsSorted(
+  events: EventType[],
+  currentDateInput: Date | string
+): EventType[] {
+  const currentDate = new Date(currentDateInput);
+
+  const nextDay = new Date(currentDate);
+  nextDay.setDate(currentDate.getDate() + 1);
+  const nextDayStr = nextDay.toISOString().split('T')[0];
+
+  return events
+    .map((event) => {
+      const normalizedAgencies = (event.agencies || []).map((agency: any) =>
+        typeof agency === 'string' ? { name: agency, logo: '' } : agency
+      );
+
+      return {
+        ...event,
+        agencies: normalizedAgencies,
+      };
+    })
+    .filter((event) => event.date === nextDayStr)
+    .sort((a, b) => {
+      const [aHour, aMin] = a.time.start.split(':').map(Number);
+      const [bHour, bMin] = b.time.start.split(':').map(Number);
+
+      const aTime = aHour * 60 + aMin;
+      const bTime = bHour * 60 + bMin;
+
+      return aTime - bTime;
+    });
+}
